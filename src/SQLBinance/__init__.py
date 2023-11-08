b1b996c0c293558d45c6a3134a5bc84bed4ec5f3
FranDorrego <francodorrego@gmail.com>
2023-11-08 14:06:50 -0300

    Arreglo de importaciones

--- src/SQLBinance/__init__.py
+++ src/SQLBinance/__init__.py
@@ -1,3 +1,15 @@
+import subprocess
+
+def instalar_libreria():
+    try:
+        import binance
+    except ImportError:
+        print("og")
+        subprocess.call(["pip", "install", "python-binance"])
+        
+instalar_libreria()
+
+
 from .classData.comprobante_data import Comprobante_data
 from .classData.transaccion_data import Transaccion_data
 from .classData._SQLBinance import _SQLBinance
@@ -5,9 +17,18 @@ from .classData.informe_data import Informe_data
 from .classData.conexion import Conexion
 import time
 
+import subprocess
+
+def instalar_libreria():
+    try:
+        import binance
+    except ImportError:
+        subprocess.call(["pip", "install", "python-binance"])
+
 class SQLBinance(_SQLBinance,Informe_data,Transaccion_data,Comprobante_data,Conexion):
 
     def __init__(self, APIkey: str | None = None, APIsecret: str | None = None, activate_bucle=True, path_db = "./base.db"):
+        instalar_libreria()
         super().__init__(APIkey, APIsecret, activate_bucle)
         Informe_data.__init__(self)
         Transaccion_data.__init__(self)
